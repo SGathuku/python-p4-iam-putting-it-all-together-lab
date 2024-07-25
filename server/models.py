72df45ed739048a1c0b349b08a48f7e9218fed35
Gathuku Samwel <gathsam3446@gmail.com>
2024-07-26 00:05:38 +0300

    Updating validation logic in models.py

--- server/models.py
+++ server/models.py
@@ -1,50 +1,53 @@
+# models.py
+
+from sqlalchemy.orm import validates, relationship
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
-
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    serialize_rules = ('-recipes.user', '-_password_hash',)
-
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    _password_hash = db.Column(db.String)
+    username = db.Column(db.String, nullable=False, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
     image_url = db.Column(db.String)
     bio = db.Column(db.String)
 
-    recipes = db.relationship('Recipe', backref='user')
+    recipes = relationship('Recipe', backref='user')
 
     @hybrid_property
     def password_hash(self):
-        raise AttributeError('Password hashes may not be viewed.')
+        raise AttributeError('Password hashes may not be accessed.')
 
     @password_hash.setter
     def password_hash(self, password):
-        password_hash = bcrypt.generate_password_hash(
-            password.encode('utf-8'))
-        self._password_hash = password_hash.decode('utf-8')
+        self._password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def authenticate(self, password):
-        return bcrypt.check_password_hash(
-            self._password_hash, password.encode('utf-8'))
+        return bcrypt.check_password_hash(self._password_hash, password)
 
-    def __repr__(self):
-        return f'<User {self.username}>'
+    @validates('username')
+    def validate_username(self, key, username):
+        assert username != '', 'Username is required.'
+        return username
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
-    __table_args__ = (
-        db.CheckConstraint('length(instructions) >= 50'),
-    )
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     instructions = db.Column(db.String, nullable=False)
-    minutes_to_complete = db.Column(db.Integer)
+    minutes_to_complete = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+    @validates('title')
+    def validate_title(self, key, title):
+        assert title != '', 'Title is required.'
+        return title
 
-    def __repr__(self):
-        return f'<Recipe {self.id}: {self.title}>'
+    @validates('instructions')
+    def validate_instructions(self, key, instructions):
+        if len(instructions) < 50:
+            raise ValueError('Instructions must be at least 50 characters long.')
+        return instructions
